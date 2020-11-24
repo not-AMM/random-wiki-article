a9cfcec562d72ec5ae7165f0de06d4dec2482b89
Alejandro M. Márquez <43765089+not-AMM@users.noreply.github.com>
2020-11-24 17:28:41 -0600

    Updated main.py with my own additions

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import wikipedia,webbrowser
-
-def getPage():
-    wikipage = wikipedia.random(1)
-    wikiload = wikipedia.page(wikipage)
-    userchoice = input("Would you like to read about {} (Y/N/Q)".format(wikipage))
-    if(userchoice == "Y" or userchoice == "y"):
-        print("\nSummary:\n--------\n" + wikipedia.summary(wikipage))
-        wikiopen = input("\nOpen Wiki page in browser? (Y/N)")
-        if(wikiopen == "Y" or wikiopen == "y"):
-            webbrowser.open(wikiload.url,new=2)
+import wikipedia
+import pyperclip
+import webbrowser
+# Works in spanish. Will work in an english version
+def gen():
+    wikipedia.set_lang("es")
+    wiki = wikipedia.random(1)
+    wpage = wikipedia.page(wiki)
+    summ = wikipedia.summary(wiki)
+    print("\nSummary:\n--------\n" + summ)
+    user = input("¿Quieres leer este tema?: (y/n//ref/q)")
+    if user == "no" or user == "n" or user == "N" or user == "No":
+            gen()
+    elif user == "si" or user == "y" or user == "s" or user == "Si":
+        open = input("¿Abrir en el naevagodor o copiar link?: (open/copy)")
+        if open == "open" or open == "abrir" or open == "o":
+            webbrowser.open(wpage.url,new=2)
+        elif open == "copy" or open == "c" or open == "Copy":
+            pyperclip.copy(wiki)
+            gen()
         else:
-            getPage()
-        exit(0)
-    elif(userchoice == "q" or userchoice == "Q"):
+            gen()
+            exit(0)
+    elif user == "q" or user == "salir" or user == "quit" or user == "Q" or user == "s":
         exit(0)
     else:
-        getPage()
+        print("*-Entrada NO aceptada-*")
+        gen()
 
-getPage()
+gen()
+
