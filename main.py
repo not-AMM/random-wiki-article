427c8c502eb36ff29afb8e11a581b31cdd3f5c21
Alejandro M. Márquez <43765089+not-AMM@users.noreply.github.com>
2020-11-24 20:34:45 -0600

    Update main.py
    Added a more readable output

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import wikipedia
 import pyperclip
 import webbrowser
-# Works in spanish. Will work in an english version
+
 def gen():
     wikipedia.set_lang("es")
     wiki = wikipedia.random(1)
     wpage = wikipedia.page(wiki)
     summ = wikipedia.summary(wiki)
-    print("\nSummary:\n--------\n" + summ)
-    user = input("¿Quieres leer este tema?: (y/n//ref/q)")
+    link = wpage.url
+# This is to give format to the results. I will work on a CLI or a minimal GUI
+    large = len(wiki)
+    division = "+----------" + ("-" * large) + "+"
+# The program starts
+    print("\nGenerando nueva entrada...\n")
+    print(division + "\n| Título: " + wiki + " |\n" + division)
+    print("| Resumen:\n+---------\n" + summ + "\n")
+    user = input("- ¿Quieres leer este tema?: (y/n/q)\n: ")
+# Replace is to avoid errors caused by a blank space
+    user = user.replace(" ", "")
     if user == "no" or user == "n" or user == "N" or user == "No":
             gen()
     elif user == "si" or user == "y" or user == "s" or user == "Si":
-        open = input("¿Abrir en el naevagodor o copiar link?: (open/copy)")
-        if open == "open" or open == "abrir" or open == "o":
+# You can choose to open the link in your default browser or just copy it into your clipboard
+        open = input("- ¿Abrir en el navegador o copiar link?: (open/copy)\n: ")
+        open = open.replace(" ", "")
+        if open == "open" or open == "abir" or open == "o" or open == "a":
             webbrowser.open(wpage.url,new=2)
         elif open == "copy" or open == "c" or open == "Copy":
-            pyperclip.copy(wiki)
+            pyperclip.copy(link)
+            print("- Título: " + wiki + "\n" + "- Link: " + link)
             gen()
         else:
             gen()
@@ -28,4 +40,3 @@ def gen():
         gen()
 
 gen()
-
